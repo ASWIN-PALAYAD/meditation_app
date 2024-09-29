4cc41382d0fa162ba072a09b39cb9aaa115a514c
ASWIN-PALAYAD <aswinpalayad07@gmail.com>
2024-09-29 17:29:16 +0530

    get started page and mediation page created

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,44 @@
-import {  Text, View } from 'react-native'
-import React from 'react'
+import { View, Text, ImageBackground } from "react-native";
+import React from "react";
+import beachImage from "@/assets/meditation-images/beach.webp";
+import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import CustomButton from "@/components/CustomButton";
+import { useRouter } from "expo-router";
+import AppGradient from "@/components/AppGradient";
 
-const index = () => {
+const App = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text className="font-bold text-red-700">home</Text>
+    <View className="flex-1 ">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <AppGradient colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}>
+          <SafeAreaView className="flex-1 px-1 justify-between">
+            <StatusBar style="light" />
+            <View className="">
+              <Text className="text-center text-white font-bold text-4xl">
+                Simple Meditation
+              </Text>
+              <Text className="text-center text-white text-xl mt-3">
+                Simplifying Meditation for Everyone
+              </Text>
+            </View>
+            <View>
+              <CustomButton
+                onPrss={() => router.push("/(tabs)/NatureMeditate")}
+                title="Get Started"
+              />
+            </View>
+          </SafeAreaView>
+        </AppGradient>
+      </ImageBackground>
     </View>
-  )
-}
+  );
+};
 
-export default index
-
+export default App;
