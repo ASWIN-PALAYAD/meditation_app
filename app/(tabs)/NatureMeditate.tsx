4dd073974783a416dc5351ee1025d930b451919a
ASWIN-PALAYAD <aswinpalayad07@gmail.com>
2024-09-29 23:20:42 +0530

    meditation screen created

--- app/(tabs)/NatureMeditate.tsx
+++ app/(tabs)/NatureMeditate.tsx
@@ -30,7 +30,7 @@ const NatureMeditate = () => {
             showsVerticalScrollIndicator={false}
             renderItem={({ item }) => (
               <Pressable
-                onPress={() => router.push(`/meditate`)}
+                onPress={() => router.push(`/meditate/${item.id}`)}
                 className="h-48 my-3 rounded-md overflow-hidden"
               >
                 <ImageBackground
